149e3126294e6ead1899fe0b0e7894075e04ad88
Duy Nguyen <duymta97@gmail.com>
2020-09-07 14:04:48 +0700

    commit first

---
+++ nhd/web/js/script.js
@@ -0,0 +1,9 @@
+require(['jquery'], function ($) {
+    'use strict';
+    // function dropdown() {
+    //     $(".dropdown").click(function () {
+    //         // $(this).siblings()
+    //         alert("Duy beo");
+    //     })
+    // }
+});
